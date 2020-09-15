2885ecbf3bce3f249490cf253bdd706e63f1a0a2
Mirza Waqas Ahmed <m.w.ahmed@gmail.com>
2020-09-15 17:07:02 -0400

    updating vs template

--- alteon-f5-config-converter.py
+++ alteon-f5-config-converter.py
@@ -89,7 +89,7 @@ real_servers_json_dumps = json.dumps(real_servers_parsed_results, sort_keys=True
 real_servers = json.loads(real_servers_json_dumps)
 # pp(real_servers[0][0]['nodes'])
 with open(runtime_vars_dir + "/real_servers_parsed_results.json", "w+",) as f:
-    f.write(json.dumps(real_servers[0][0]))
+    f.write(json.dumps(real_servers[0][0], indent=4, sort_keys=True))
 
 with open('./templates/groups_ttp.xml', 'r') as groups_ttp:
     groups_ttp_template = groups_ttp.read()
@@ -105,7 +105,7 @@ groups_json_dumps = json.dumps(groups_parsed_results, sort_keys=True,
 groups = json.loads(groups_json_dumps)
 # pp(groups[0][0]['pools'])
 with open(runtime_vars_dir + "/groups_parsed_results.json", "w+",) as f:
-    f.write(json.dumps(groups[0][0]))
+    f.write(json.dumps(groups[0][0], indent=4, sort_keys=True))
 
 with open('./templates/virtual_servers_ttp.xml', 'r') as virtual_servers_ttp:
     virtual_servers_ttp_template = virtual_servers_ttp.read()
@@ -122,7 +122,7 @@ virtual_servers_json_dumps = json.dumps(virtual_servers_parsed_results, sort_key
 virtual_servers = json.loads(virtual_servers_json_dumps)
 # pp(virtual_servers[0][0]['virt'])
 with open(runtime_vars_dir + "/virtual_servers_parsed_results.json", "w+",) as f:
-    f.write(json.dumps(virtual_servers[0][0]))
+    f.write(json.dumps(virtual_servers[0][0], indent=4, sort_keys=True))
 
 with open('./templates/ssl_ttp.xml', 'r') as ssl_ttp:
     ssl_ttp_template = ssl_ttp.read()
@@ -141,16 +141,17 @@ ssl = json.loads(ssl_json_dumps)
 # Print pbind_cookie results
 # pp(ssl[0][0]['pbind_cookie'])
 with open(runtime_vars_dir + "/pbind_cookie_parsed_results.json", "w+",) as f:
-    f.write(json.dumps(ssl[0][0]))
+    f.write(json.dumps(ssl[0][0], indent=4, sort_keys=True))
 
 # Print pbind_sslid results
 # pp(ssl[1][0]['pbind_sslid'])
 with open(runtime_vars_dir + "/pbind_sslid_parsed_results.json", "w+",) as f:
-    f.write(json.dumps(ssl[1][0]))
+    f.write(json.dumps(ssl[1][0], indent=4, sort_keys=True))
 
 nodes = real_servers[0][0]['nodes']
 pools = groups[0][0]['pools']
 vips = virtual_servers[0][0]['virt']
+
 pbind_cookie = ssl[0][0]['pbind_cookie']
 if not isinstance(pbind_cookie, list):
     pbind_cookie = [pbind_cookie]
@@ -177,7 +178,7 @@ ssl_cert_bundle_json_dumps = json.dumps(ssl_cert_bundle_parsed_results, sort_key
 ssl_cert_bundle = json.loads(ssl_cert_bundle_json_dumps)
 
 with open(runtime_vars_dir + "/ssl_cert_bundle_parsed_results.json", "w+",) as f:
-    f.write(json.dumps(ssl_cert_bundle[0][0]))
+    f.write(json.dumps(ssl_cert_bundle[0][0], indent=4, sort_keys=True))
 
 with open('./templates/ssl_certs_ttp.xml', 'r') as ssl_certs_ttp:
     ssl_certs_ttp_template = ssl_certs_ttp.read()
@@ -194,7 +195,7 @@ ssl_certs_json_dumps = json.dumps(ssl_certs_parsed_results, sort_keys=True,
 ssl_certs = json.loads(ssl_certs_json_dumps)
 
 with open(runtime_vars_dir + "/ssl_certs_parsed_results.json", "w+",) as f:
-    f.write(json.dumps(ssl_certs[0][0]))
+    f.write(json.dumps(ssl_certs[0][0], indent=4, sort_keys=True))
 
 with open('./templates/ssl_policies_ttp.xml', 'r') as ssl_policies_ttp:
     ssl_policies_ttp_template = ssl_policies_ttp.read()
@@ -211,31 +212,34 @@ ssl_policies_json_dumps = json.dumps(ssl_policies_parsed_results, sort_keys=True
 ssl_policies = json.loads(ssl_policies_json_dumps)
 
 with open(runtime_vars_dir + "/ssl_policies_parsed_results.json", "w+",) as f:
-    f.write(json.dumps(ssl_policies[0][0]))
+    f.write(json.dumps(ssl_policies[0][0], indent=4, sort_keys=True))
 
 ##############Till here data is parsed###############
 
 #### Below function is now combining vip relevant data only...####
 # Updating the VIPS Dictionary with cookie and sslid; pbind settings
 for vip, vip_config in vips.items():
-    for vip_port, config in vip_config['services'].items():
-        for cookie in pbind_cookie:
-            try:
-                if config['virt_seq'] == cookie['virt_seq'] and vip_port == cookie['vip_port']:
-                    config['pbind'] = cookie['pbind']
-                    config['pbind_type'] = cookie['pbind_type']
-                    config['rcount'] = cookie['rcount']
-            except:
-                extype, value, tb = sys.exc_info()
-                traceback.print_exc()
-                pdb.post_mortem(tb)
-        for sslid in pbind_sslid:
-            if config['virt_seq'] == sslid['virt_seq'] and vip_port == sslid['vip_port']:
-                config['pbind'] = sslid['pbind']
+    if isinstance(vip_config,list):
+        print(f"There are multiple /virt configs for {vip}.\n change the IP to something else to fix this later revert it in F5 config.")
+    elif isinstance(vip_config,dict):
+        for vip_port, config in vip_config['services'].items():
+            for cookie in pbind_cookie:
+                try:
+                    if config['virt_seq'] == cookie['virt_seq'] and vip_port == cookie['vip_port']:
+                        config['pbind'] = cookie['pbind']
+                        config['pbind_type'] = cookie['pbind_type']
+                        config['rcount'] = cookie['rcount']
+                except:
+                    extype, value, tb = sys.exc_info()
+                    traceback.print_exc()
+                    pdb.post_mortem(tb)
+            for sslid in pbind_sslid:
+                if config['virt_seq'] == sslid['virt_seq'] and vip_port == sslid['vip_port']:
+                    config['pbind'] = sslid['pbind']
 
-# Declaring a new object vips for later use in the program
-# vips = vips
-
+# Saving new VIPs dict to file
+with open(runtime_vars_dir + "/vips_including_ssl.json", "w+",) as f:
+    f.write(json.dumps(vips, indent=4, sort_keys=True))
 # pp(nodes)
 # pp(pools)
 # pp(vips)
@@ -274,10 +278,10 @@ for node in nodes:
 # pp(nodes_seq_dict)
 # pp(nodes_name_dict)
 with open(runtime_vars_dir + "/nodes_seq_dict.json", "w+",) as f:
-    f.write(json.dumps(nodes_seq_dict))
+    f.write(json.dumps(nodes_seq_dict, indent=4, sort_keys=True))
 
 with open(runtime_vars_dir + "/nodes_name_dict.json", "w+",) as f:
-    f.write(json.dumps(nodes_name_dict))
+    f.write(json.dumps(nodes_name_dict, indent=4, sort_keys=True))
 
 pools_seq_dict = {}
 pools_name_dict = {}
@@ -286,25 +290,26 @@ for pool in pools:
     _ = pools_name_dict.update({pool['group_name']: pool})
 
 with open(runtime_vars_dir + "/pools_seq_dict.json", "w+",) as f:
-    f.write(json.dumps(pools_seq_dict))
+    f.write(json.dumps(pools_seq_dict, indent=4, sort_keys=True))
 
 with open(runtime_vars_dir + "/pools_name_dict.json", "w+",) as f:
-    f.write(json.dumps(pools_name_dict))
+    f.write(json.dumps(pools_name_dict, indent=4, sort_keys=True))
 
 vips_vip_ip_dict = {}
 vips_vip_seq_dict = {}
 with open(runtime_vars_dir + "/vips_vip_ip_dict.json", "w+",) as f:
-    f.write(json.dumps(virtual_servers[0][0]['virt']))
+    f.write(json.dumps(virtual_servers[0][0]['virt'], indent=4, sort_keys=True))
 
 for vip, config in vips.items():
-    _ = vips_vip_seq_dict.update({config['virt_seq']: config})
-    # _ = vips_vip_seq_dict['virt_seq'].update(
-    #     {'vip_ip': {}})
-    _ = vips_vip_seq_dict[config['virt_seq']].update(
-        {'vip_ip': vip})
+    if isinstance(config,dict):
+        _ = vips_vip_seq_dict.update({config['virt_seq']: config})
+        # _ = vips_vip_seq_dict['virt_seq'].update(
+        #     {'vip_ip': {}})
+        _ = vips_vip_seq_dict[config['virt_seq']].update(
+            {'vip_ip': vip})
 
 with open(runtime_vars_dir + "/vips_vip_seq_dict.json", "w+",) as f:
-    f.write(json.dumps(vips_vip_seq_dict))
+    f.write(json.dumps(vips_vip_seq_dict, indent=4, sort_keys=True))
 
 pbind_cookie_vip_seq_dict = {}
 pbind_cookie_vip_service_dict = {}
@@ -313,17 +318,17 @@ for pbind in pbind_cookie:
     _ = pbind_cookie_vip_service_dict.update({pbind['vip_port']: pbind})
 
 with open(runtime_vars_dir + "/pbind_cookie_vip_seq_dict.json", "w+",) as f:
-    f.write(json.dumps(pbind_cookie_vip_seq_dict))
+    f.write(json.dumps(pbind_cookie_vip_seq_dict, indent=4, sort_keys=True))
 
 with open(runtime_vars_dir + "/pbind_cookie_vip_service_dict.json", "w+",) as f:
-    f.write(json.dumps(pbind_cookie_vip_service_dict))
+    f.write(json.dumps(pbind_cookie_vip_service_dict, indent=4, sort_keys=True))
 
 pbind_sslid_vip_seq_dict = {}
 for pbind in pbind_sslid:
     _ = pbind_sslid_vip_seq_dict.update({pbind['virt_seq']: pbind})
 
 with open(runtime_vars_dir + "/pbind_sslid_vip_seq_dict.json", "w+",) as f:
-    f.write(json.dumps(pbind_sslid_vip_seq_dict))
+    f.write(json.dumps(pbind_sslid_vip_seq_dict, indent=4, sort_keys=True))
 
 ############ END Creating helper data ############
 
@@ -356,7 +361,7 @@ if alteon_state_dump:
                                                     indent=4, separators=(',', ': '))
     real_servers_state_dump = json.loads(real_servers_state_dump_json_dumps)
     with open(runtime_vars_dir + "/real_servers_state_dump_parser_results.json", "w+",) as f:
-        f.write(json.dumps(real_servers_state_dump[0][0]))
+        f.write(json.dumps(real_servers_state_dump[0][0], indent=4, sort_keys=True))
 
     nodes_state = real_servers_state_dump[0][0]['nodes_state']
 
@@ -375,7 +380,7 @@ if alteon_state_dump:
     virtual_servers_state_dump = json.loads(
         virtual_servers_state_dump_json_dumps)
     with open(runtime_vars_dir + "/virtual_servers_state_dump_parser_results.json", "w+",) as f:
-        f.write(json.dumps(virtual_servers_state_dump[0][0]))
+        f.write(json.dumps(virtual_servers_state_dump[0][0], indent=4, sort_keys=True))
 
     vips_state = virtual_servers_state_dump[0][0]['vips_state']
 
@@ -390,6 +395,7 @@ if alteon_state_dump:
 def model_alteon_data(vips):
     all_vips = []
     for vip, vip_config in vips.items():
+        # print(vip)
         if vip_config['config_state'] == 'ena':
             for vip_port, srv_config in vip_config['services'].items():
                 vip_dict = dict({
@@ -403,26 +409,22 @@ def model_alteon_data(vips):
                         "lb_method": "",
                         "members": []
                     }})
-                _ = vip_dict.update({'virt_seq': srv_config["virt_seq"]})
-                _ = vip_dict.update({'vip_ip': vip})
-                _ = vip_dict.update({'vip_port': vip_port})
+                vip_dict['virt_seq'] = srv_config["virt_seq"]
+                vip_dict['vip_ip'] = vip
+                vip_dict['vip_port'] = vip_port
                 if vip_config['vip_name'] != 'None':
                     vip_name = regex_replace(
                         vip_config['vip_name'], "[^A-Za-z0-9-_.]", "")
-                    _ = vip_dict.update(
-                        {'vip_name': "vs-tcp-" + str(vip_port) + "-" + vip_name + "-" + str(vip)})
-                    _ = vip_dict['pool'].update(
-                        {'name': "pool-tcp-" + str(vip_port) + "-" + vip_name + "-" + str(vip)})
+                    vip_dict['vip_name'] = "vs-tcp-" + str(vip_port) + "-" + vip_name + "-" + str(vip)
+                    vip_dict['pool']['name'] = "pool-tcp-" + str(vip_port) + "-" + vip_name + "-" + str(vip)
                 elif vip_config['vip_name'] == 'None':
                     group_name = pools_seq_dict[srv_config['group_seq']
                                                 ]['group_name']
                     if group_name != 'None':
                         vip_name = regex_replace(
                             group_name, "[^A-Za-z0-9-_.]", "")
-                        _ = vip_dict.update(
-                            {'vip_name': "vs-tcp-" + str(vip_port) + "-" + vip_name + "-" + str(vip)})
-                        _ = vip_dict['pool'].update(
-                            {'name': "pool-tcp-" + str(vip_port) + "-" + vip_name + "-" + str(vip)})
+                        vip_dict['vip_name'] = "vs-tcp-" + str(vip_port) + "-" + vip_name + "-" + str(vip)
+                        vip_dict['pool']['name'] = "pool-tcp-" + str(vip_port) + "-" + vip_name + "-" + str(vip)
                     elif group_name == 'None':
                         node_list = pools_seq_dict[srv_config['group_seq']
                                                    ]['node_list']
@@ -431,33 +433,25 @@ def model_alteon_data(vips):
                                                        ]['node_name']
                             if node_name != 'None':
                                 vip_name = node_name
-                                _ = vip_dict.update(
-                                    {'vip_name': "vs-tcp-" + str(vip_port) + "-" + vip_name + "-" + str(vip)})
-                                _ = vip_dict['pool'].update(
-                                    {'name': "pool-tcp-" + str(vip_port) + "-" + vip_name + "-" + str(vip)})
+                                vip_dict['vip_name'] = "vs-tcp-" + str(vip_port) + "-" + vip_name + "-" + str(vip)
+                                vip_dict['pool']['name'] = "pool-tcp-" + str(vip_port) + "-" + vip_name + "-" + str(vip)
                                 break
                             elif node_name == 'None' and i != len(node_list) - 1:
                                 continue
                             else:
                                 vip_name = "migrated-from-alteon"
-                                _ = vip_dict.update(
-                                    {'vip_name': "vs-tcp-" + str(vip_port) + "-" + vip_name + "-" + str(vip)})
-                                _ = vip_dict['pool'].update(
-                                    {'name': "pool-tcp-" + str(vip_port) + "-" + vip_name + "-" + str(vip)})
+                                vip_dict['vip_name'] = "vs-tcp-" + str(vip_port) + "-" + vip_name + "-" + str(vip)
+                                vip_dict['pool']['name'] = "pool-tcp-" + str(vip_port) + "-" + vip_name + "-" + str(vip)
                     else:
                         vip_name = "unknown-vip_name-fixme"
-                        _ = vip_dict.update(
-                            {'vip_name': "vs-tcp-" + str(vip_port) + "-" + vip_name + "-" + str(vip)})
-                        _ = vip_dict['pool'].update(
-                            {'name': "pool-tcp-" + str(vip_port) + "-" + vip_name + "-" + str(vip)})
+                        vip_dict['vip_name'] = "vs-tcp-" + str(vip_port) + "-" + vip_name + "-" + str(vip)
+                        vip_dict['pool']['name'] = "pool-tcp-" + str(vip_port) + "-" + vip_name + "-" + str(vip)
                 lb_method = pools_seq_dict[srv_config['group_seq']
                                            ]['lb_method']
                 if lb_method == 'roundrobin':
-                    _ = vip_dict['pool'].update(
-                        {'lb_method': "round-robin"})
+                    vip_dict['pool']['lb_method'] = "round-robin"
                 elif lb_method == 'None':
-                    _ = vip_dict['pool'].update(
-                        {'lb_method': "least-connections-member"})
+                    vip_dict['pool']['lb_method'] = "least-connections-member"
                 nodes_list = pools_seq_dict[srv_config['group_seq']
                                             ]['node_list']
                 for node in nodes_list:
@@ -467,7 +461,7 @@ def model_alteon_data(vips):
 
                     node_name = regex_replace(
                         nodes_seq_dict[node_seq]['node_name'], "[^A-Za-z0-9-_.]", "")
-                    _ = vip_dict['pool']['members'].append({
+                    vip_dict['pool']['members'].append({
                         'node_name': node_name if node_name != 'None' else vip_dict['vip_name'] + "_real_server_" + str(node_seq),
                         'node_ip': node_ip,
                         'node_port': srv_config['real_port'] if srv_config['real_port'] != 'None' else vip_port
@@ -475,39 +469,37 @@ def model_alteon_data(vips):
                 if 'pbind' in srv_config.keys() and 'ssl_profile' not in srv_config.keys():
                     if srv_config['pbind'] != "None":
                         if srv_config['pbind'] == "sslid":
-                            _ = vip_dict.update({'vip_type': "b"})
+                            vip_dict['vip_type'] = "b"
                         elif srv_config['pbind'] == "clientip":
-                            _ = vip_dict.update({'vip_type': "e"})
+                            vip_dict['vip_type'] = "e"
                         else:
-                            _ = vip_dict.update({'vip_type': "a"})
+                            vip_dict['vip_type'] = "a"
                     else:
-                        _ = vip_dict.update({'vip_type': "a"})
+                        vip_dict['vip_type'] = "a"
                 elif 'pbind' in srv_config.keys() and 'ssl_profile' in srv_config.keys():
                     if srv_config['pbind'] != "None":
                         if srv_config['pbind'] == "cookie":
-                            _ = vip_dict.update({'vip_type': "c"})
-                            _ = vip_dict.update(
-                                {'ssl_profile': srv_config['ssl_profile']})
-                            _ = vip_dict['ssl_profile'].update({
+                            vip_dict['vip_type'] = "c"
+                            vip_dict['ssl_profile'] = srv_config['ssl_profile']
+                            vip_dict['ssl_profile'].update({
                                 'persistence': srv_config['pbind'],
                                 'persist_cookie': srv_config['pbind_type'],
                             })
                         else:
-                            _ = vip_dict.update({'vip_type': "a"})
+                            vip_dict['vip_type'] = "a"
                     else:
-                        _ = vip_dict.update({'vip_type': "a"})
+                        vip_dict['vip_type'] = "a"
                 elif srv_config['dbind'] == "ena":
-                    _ = vip_dict.update({'vip_type': "a"})
+                    vip_dict['vip_type'] = "a"
 
                 elif srv_config['dbind'] == "None":
-                    _ = vip_dict.update({'vip_type': "d"})
+                    vip_dict['vip_type'] = "d"
 
                 else:
-                    _ = vip_dict.update({'vip_type': "a"})
+                    vip_dict['vip_type'] = "a"
 
-                _ = all_vips.append(vip_dict)
+                all_vips.append(vip_dict)
     return all_vips
-
 
 # VIP Types - reference only
 # If dbind then use standard vip
